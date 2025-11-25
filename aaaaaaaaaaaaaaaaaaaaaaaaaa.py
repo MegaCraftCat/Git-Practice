a0e7cbae796373d6746cd0b3372de10f6795d34a
MegaCraftCat <141450132+MegaCraftCat@users.noreply.github.com>
2025-11-25 15:06:08 -0500

    abcdefghiujkgiuulrehnnnnnnnnwl
    no

--- aaaaaaaaaaaaaaaaaaaaaaaaaa.py
+++ aaaaaaaaaaaaaaaaaaaaaaaaaa.py
@@ -1,1 +1,15 @@
-print("aaaaaaaaaaaaaaaaaaaaaa")
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaA, B, C, D, E, F, G
+H, I, J, K, L, M, N, O, P
+Q, R, S, T, U, V
+W, X, Y, Z
+
+Now I know my ABCs.
+Next time won’t you sing with me?
+
+A, B, C, D, E, F, G
+H, I, J, K, L, M, N, O, P
+Q, R, S, T, U, V
+W, X, Y, and Z
+
+Now I know my ABCs.
+Next time won’t you sing with me?")
